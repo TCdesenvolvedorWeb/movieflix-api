00513971bd60e7f2a3edd6da76c5cee5153fb155
Dev-Tadeu <tadeutrabalho26@gmail.com>
2025-01-28 13:17:07 -0300

    :sparkles: feat: cadastra novos filmes

--- server.ts
+++ server.ts
@@ -5,7 +5,9 @@ const port: number = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (req: any, res: any) => {
+app.use(express.json());
+
+app.get("/movies", async (_: any, res: any) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
       title: "asc",
@@ -13,9 +15,29 @@ app.get("/movies", async (req: any, res: any) => {
     include: {
       genres: true,
       languages: true,
-    }
+    },
   });
   res.json(movies);
+});
+
+app.post("/movies", async (req: any, res: any) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        realese_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao cadastrar um filme." });
+  }
+
+  res.status(201).send();
 });
 
 app.listen(port, () => {
